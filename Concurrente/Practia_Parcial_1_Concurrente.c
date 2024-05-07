9e60009182e2cde56f3820997362d8bf2ba7a9ad
Ignacio <meloignacionicolas@gmail.com>
2024-05-07 11:20:21 -0300

    mas practica concurrente

--- Concurrente/Practia_Parcial_1_Concurrente.c
+++ Concurrente/Practia_Parcial_1_Concurrente.c
@@ -330,3 +330,310 @@ Process Repositor{
 //Resolver el siguiente problema con MONITORES. En una montaña hay 30 escaladores que en una parte de la subida 
 // deben utilizar un único paso de a uno a la vez y de acuerdo al orden de llegada al mismo. Nota: sólo se pueden 
 // utilizar procesos que representen a los escaladores; cada escalador usa sólo una vez el paso
+
+Process escalador[id:1..30]{
+    subida.utilizar(id);
+    subida.salir();
+}
+
+Monitor subida{
+    cond espera;
+    boolean libre=true;
+    int esperando=0;
+
+    Procedure utilizar(id:int in){
+        if(!libre){
+            esperando++;
+            await(espera);
+        } ;
+        else{
+            libre=false;
+        }
+    }
+    procedure salir(){
+        if(esperando>0){
+            signal(espera);
+            esperando--;
+        }
+        else{
+            libre=true;
+        }
+    }
+
+}
+// PRACTICA 2 Semaforos
+
+// 2. Un sistema de control cuenta con 4 procesos que realizan chequeos en forma 
+// colaborativa. Para ello, reciben el historial de fallos del día anterior (por simplicidad, de 
+// tamaño N). De cada fallo, se conoce su número de identificación (ID) y su nivel de 
+// gravedad (0=bajo, 1=intermedio, 2=alto, 3=crítico). Resuelva considerando las siguientes 
+// situaciones:
+// a) Se debe imprimir en pantalla los ID de todos los errores críticos (no importa el 
+// orden).
+// b) Se debe calcular la cantidad de fallos por nivel de gravedad, debiendo quedar los 
+// resultados en un vector global.
+// c) Ídem b) pero cada proceso debe ocuparse de contar los fallos de un nivel de 
+// gravedad determinado.
+
+cola historial;
+sem mutexH=1;
+int cant=N;
+int termino=0;
+sem terminaron=1;
+sem espera;
+int vector[4]([4]0);
+sem vector;//esto es necesario en el b, en el c cada uno se ocupa de uno solo, por lo que 1 solo proceso accede a esa posicion del vector.
+Process chequeos[id:1..4]{
+    int id;
+    int fallo;
+    P(mutexH)
+    while(cant>0){
+        id,fallo=historial.pop();
+        cant--;
+        V(mutexH);
+        if(fallo==3){
+            print(id);
+        }
+        if(fallo==id){
+            vector[id]++;
+        }
+        P(mutexH)
+    }
+    V(mutexH)
+    P(terminaron)
+    termino++;
+    if(termino==4){
+        V(terminaron)
+        for i=1;i<4;i++{
+            V(espera)
+        }
+        P(espera)
+    }
+    else{
+        V(terminaron)
+        P(espera)
+    }
+}
+
+//4. Suponga que existe una BD que puede ser accedida por 6 usuarios como máximo al 
+// mismo tiempo. Además, los usuarios se clasifican como usuarios de prioridad alta y 
+// usuarios de prioridad baja. Por último, la BD tiene la siguiente restricción:
+// • no puede haber más de 4 usuarios con prioridad alta al mismo tiempo usando la BD.
+// • no puede haber más de 5 usuarios con prioridad baja al mismo tiempo usando la BD.
+// Indique si la solución presentada es la más adecuada. Justifique la respuesta. 
+
+sem limitedb=6;
+sem limiteBaja=5;
+sem limiteAlta=4;
+
+Process usuarioAlta[id:1..n]{
+    P(limiteAlta)
+    P(limitedb)
+    //accede
+    V(limitedb)
+    V(limiteAlta)
+
+}
+Process usuarioBaja[id:1..N]{
+    P(limiteBaja)
+    P(limitedb)
+    //accede
+    V(limitedb)
+    V(limiteBaja)
+}
+
+// En una estación de trenes, asisten P personas que deben realizar una carga de su tarjeta SUBE en la terminal 
+// disponible. La terminal es utilizada en forma exclusiva por cada persona de acuerdo con el orden de llegada. 
+// Implemente una solución utilizando sólo emplee procesos Persona. Nota: la función UsarTerminal() le permite cargar 
+// la SUBE en la terminal disponible.
+
+sem accessTerminal[P];
+cola llegada;
+sem mutexC=1;
+boolean libre=true;
+int esperando=0;
+sem espera=1;
+
+Process persona[id:1..P]{
+    if(!libre){
+        P(espera)
+        esperando++;
+        V(espera)
+        P(mutexC)
+        llegada.push(id);
+        V(mutexC)
+        P(accessTerminal[id]);
+    }
+    else{
+        libre=false;
+    }
+    UsarTerminal();
+    P(espera)
+    if(esperando>0){
+        P(mutexC)
+        V(accessTerminal[llegada.pop()]);
+        V(mutexC)
+        esperando--;
+    }
+    else{
+        libre=true;
+    }
+    V(espera)
+}
+//8min
+
+// Resuelva el mismo problema anterior pero ahora considerando que hay T terminales disponibles. Las personas 
+// realizan una única fila y la carga la realizan en la primera terminal que se libera. Recuerde que sólo debe emplear 
+// procesos Persona. Nota: la función UsarTerminal(t) le permite cargar la SUBE en la terminal t
+
+cola terminales;//tiene las T terminales ya cargadas
+sem accessTe=1;
+int terminales=T;
+sem contT=1;
+sem espera[P]([P]0);
+cola esperando;
+sem mutexEsp=1;
+
+Process persona[id:1..P]{
+    int t;
+    P(contT)
+    if(terminales==0){
+        V(contT)
+        P(mutexEsp)
+        esperando.push(id);
+        V(mutexEsp)
+        P(espera[id]);
+    }
+    else{
+        P(contT)
+        terminales--;
+        V(contT)
+    }
+    P(accessTe)
+    t=terminales.push()
+    V(accessTe)
+    UsarTerminal(t);
+    P(accessTe)
+    terminales.pop(t)
+    V(accessTe)
+    P(mutexEsp)
+    if(!esperando.isEmpty()){
+        V(espera[esperando.push()])
+        V(mutexEsp)
+    }
+    else{
+        V(mutexEsp)
+        P(contT)
+        terminales++;
+        V(contT)
+    }
+}
+//12min
+
+// Resolver con MONITORES el siguiente problema: En una elección estudiantil, se utiliza una máquina para voto 
+// electrónico. Existen N Personas que votan y una Autoridad de Mesa que les da acceso a la máquina de acuerdo con el 
+// orden de llegada, aunque ancianos y embarazadas tienen prioridad sobre el resto. La máquina de voto sólo puede ser 
+// usada por una persona a la vez. Nota: la función Votar() permite usar la máquina
+
+Process Persona[id:1..N]{
+    int prioridad=Prioridad();
+    maquina.entrada(id,prioridad);
+    Votar();
+    maquina.salida()
+}
+Process Autoridad{
+    while(true){
+        maquina.siguiente();
+    }
+}
+Monitor maquina{
+    cola espera;//cola que depende de la prioridad se ordena la misma
+    cond esperando[N];
+    cond salio;
+    cond entro;
+    Procedure entrada(id:int in;prioridad: int in){
+        espera.push(id,prioridad);
+        signal(entro);
+        await(esperando[id]);
+    }
+    Procedure salida(){
+        signal(salio);
+
+    }
+    Procedure siguiente(){
+        if(!espera.isEmpty())-> await(entro);
+        signal(esperando[espera.pop()]);
+        await(salio);
+    }
+}
+//12mins
+
+
+// Resolver con SEMÁFOROS el siguiente problema. En una planta verificadora de vehículos, existen 7 estaciones donde 
+// se dirigen 150 vehículos para ser verificados. Cuando un vehículo llega a la planta, el coordinador de la planta le 
+// indica a qué estación debe dirigirse. El coordinador selecciona la estación que tenga menos vehículos asignados en ese 
+// momento. Una vez que el vehículo sabe qué estación le fue asignada, se dirige a la misma y espera a que lo llamen 
+// para verificar. Luego de la revisión, la estación le entrega un comprobante que indica si pasó la revisión o no. Más allá 
+// del resultado, el vehículo se retira de la planta. Nota: maximizar la concurrencia. 
+
+sem contadorEstaciones[7]([7]1);
+int estaciones[7]([7]0);//array que tiene la cantidad de autos de cada estacion, cuenta con la funcion de conseguir el indice del que menor cantidad tiene.
+cola autosEsperando;
+sem mutexC=1;
+sem hayAuto=0;
+int estacionDesignada[150]([150]0);
+sem estacionAsignada[150]([150]0);
+sem estacionAtendiendo[7]([7]1);
+int atendeme[7]([7]0);
+sem podesDejarTuId[7]=([7]1);
+int respuesta[150]([150]0);
+sem respuestaEnviada[150]([150]0);
+
+
+Process vehiculo[id:1..150]{
+    int idEstacion;
+    int r;
+    P(mutexC)
+    autosEsperando.push(id);
+    V(mutexC)
+    V(hayAuto);
+    P(estacionAsignada[id]);
+    idEstacion=estacionDesignada[id];
+
+    V(podesDejarTuId[idEstacion]);
+    atendeme[idEstacion]=id;
+    V(estacionAtendiendo[idEstacion]);
+    P(respuestaEnviada[id]);
+    r=respuestaEnviada[id];
+    P(contadorEstaciones[idEstacion])
+    estaciones[idEstacion]--;
+    V(contadorEstaciones[idEstacion]);
+}
+
+Process coordinador{
+    int idAtender;
+    while(true){
+        P(mutexC)
+        if(autosEsperando.isEmpty()){
+            V(mutexC)
+            P(hayAuto);
+        } 
+        idAtender=autosEsperando.pop();
+        V(mutexC)
+        estacionDesignada[idAtender]=estaciones.menorCantidadEsperando();
+        V(estacionAsignada[idAtender]);
+    }
+}
+Process estacion[id:1..7]{
+    int idAtender;
+    int r;
+    while(true){
+        P(estacionAtendiendo[id]);
+        idAtender=atendeme[id];
+        r=atender(idAtender);
+        respuestaEnviada[idAtender]=r;
+        V(respuestaEnviada[idAtender]);
+        V(podesDejarTuId[id]);
+    }
+}
+//22mins aprox.
